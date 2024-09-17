b55bb567d2ff0263b167e70227d73b1c99772c62
Harsh Chouhan <84805840+harshchouhan3122@users.noreply.github.com>
2024-09-17 13:25:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default {
   server: {
     port: process.env.PORT || 5173,  // Use the PORT environment variable or fallback to 5173
     host: true                       // Expose the server to external connections
   }
-};
+})
+
